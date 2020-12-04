7fd78353795e76ca6749f878cfad3ebff6cb3bdc
つぐみ 馬場 <180562@jc-21.jp>
2020-12-04 10:51:56 +0900

    課題11,12

---
+++ src/PageAD4Servlet.java
@@ -0,0 +1,71 @@
+
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Page1Servlet
+ */
+@WebServlet("/pageAD4")
+public class PageAD4Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PageAD4Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		final String driverName = "oracle.jdbc.driver.OracleDriver";
+		final String url = "jdbc:oracle:thin:@192.168.54.226:1521/orcl";
+		final String id = "OUBO";
+		final String pass = "TOUSEN";
+		
+		String sNumA = request.getParameter("numa");
+		String sNumB = request.getParameter("numb");
+
+		try {
+			Class.forName(driverName);
+			Connection connection=DriverManager.getConnection(url,id,pass);
+			PreparedStatement st = 
+					connection.prepareStatement(
+							"Insert into TOUSEN(NUMA,NUMB) Values(?,?)"
+						);
+			st.setString(1, sNumA);
+			st.setString(2, sNumB);
+			st.executeUpdate();
+							
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/JSP/pageAD4.jsp");
+			rd.forward(request, response);
+			
+		}catch(SQLException e) {
+			System.out.println("SQLException");
+			response.getWriter().println("SQLException");
+			e.printStackTrace();
+			e.printStackTrace(response.getWriter());
+		} catch (ClassNotFoundException e) {
+			System.out.println("ClassNotFoundException");
+			response.getWriter().println("ClassNotFoundException");
+			e.printStackTrace();
+			e.printStackTrace(response.getWriter());
+		}
+		
+	}
+
+}
